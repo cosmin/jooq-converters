26fc09fb2d84692d1196bae85d4308e473ee02c3
gpeck <peck.del@gmail.com>
2020-04-28 16:04:00 -0700

    Update PostgresJSONJacksonJsonNodeBinding.java

--- src/main/java/com/offbytwo/jooq/converters/PostgresJSONJacksonJsonNodeBinding.java
+++ src/main/java/com/offbytwo/jooq/converters/PostgresJSONJacksonJsonNodeBinding.java
@@ -9,10 +9,10 @@ import java.sql.Types;
 
 import static org.jooq.tools.Convert.convert;
 
-public class PostgresJSONJacksonJsonNodeBinding implements Binding<JSON, JsonNode> {
+public class PostgresJSONJacksonJsonNodeBinding implements Binding<JSONB, JsonNode> {
 
     @Override
-    public Converter<JSON, JsonNode> converter() {
+    public Converter<JSONB, JsonNode> converter() {
         return new PostgresJSONJacksonJsonNodeConverter();
     }
 
@@ -37,12 +37,12 @@ public class PostgresJSONJacksonJsonNodeBinding implements Binding<JSON, JsonNod
 
     @Override
     public void get(BindingGetResultSetContext<JsonNode> ctx) throws SQLException {
-        ctx.convert(converter()).value(JSON.valueOf(ctx.resultSet().getString(ctx.index())));
+        ctx.convert(converter()).value(JSONB.valueOf(ctx.resultSet().getString(ctx.index())));
     }
 
     @Override
     public void get(BindingGetStatementContext<JsonNode> ctx) throws SQLException {
-        ctx.convert(converter()).value(JSON.valueOf(ctx.statement().getString(ctx.index())));
+        ctx.convert(converter()).value(JSONB.valueOf(ctx.statement().getString(ctx.index())));
     }
 
     // The below methods aren't needed in PostgreSQL:
